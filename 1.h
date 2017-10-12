baab41e5b1aa4a8a960e739eef9b18079cbb0b6e
vench <nowwrit@mail.ru>
2017-10-12 23:02:22 +0300

    first

---
+++ 1.h
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   1.h
+ * Author: vench
+ *
+ * Created on 8 октября 2017 г., 22:57
+ */
+
+
+
+#ifndef ONE_H
+#define ONE_H
+
+ enum MyEnum{My1, My2, My3};
+
+#endif /* 1_H */
+
