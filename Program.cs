5abb37469e5a283c01ee95e30ec9a1f8570cfe2e
vench <nowwrit@mail.ru>
2018-09-25 09:21:06 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        
+            Console.WriteLine("Press any key to exit.");
+           // Console.ReadKey();
+          //  char s = Console.ReadKey().KeyChar;
+           // Console.WriteLine("S -> ", s);
+
+        
+        }
+
+    }
+}
