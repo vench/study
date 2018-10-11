8d3691eb8d06fa64f11bca6ab7a8ca277af115d4
vench <nowwrit@mail.ru>
2018-10-11 19:16:13 +0300

    1110

--- MyForm.cs
+++ MyForm.cs
@@ -15,35 +15,64 @@ namespace myApp {
 
     class MyForm : Form {
 
+
+        enum actions  {DOT, CLEAR, DEFAULT};
+
         const int MIN_WIDTH = 640;
         const int MIN_HEIGHT = 480;
+
+        List<PointF> listPoints;
+
+        private actions mode;
+
+        private Rectangle rectOfDraw;
+ 
 
         public MyForm()
         {
 
+            listPoints = new  List<PointF>();   
+            
+            // SET EVENTS
             Paint += new PaintEventHandler(Form1_Paint);
+            this.Click += new EventHandler (Form1_Click); 
 
             initializeComponent();
 
-            Text = "Main frame";
+            Text = "Рисуем линии";
             SetBounds(0,0, MIN_WIDTH, MIN_HEIGHT);
             MaximumSize = new System.Drawing.Size(SystemInformation.VirtualScreen.Width,SystemInformation.VirtualScreen.Height);
             MinimumSize = new System.Drawing.Size(MIN_WIDTH, MIN_HEIGHT);
             StartPosition = FormStartPosition.CenterScreen;
         }
 
-        void Form1_Paint(object sender,PaintEventArgs e) {
-            Graphics g = e.Graphics;
-            
-            // Set up the arc.  
-   var p = new Pen(Color.MediumBlue, 3);
-   var r = new Rectangle(100, 100, 200, 100);
-   float startAngle = 0.0f;
-   float sweepAngle = 90.0f;
+        private void Form1_Paint(object sender,PaintEventArgs e) {
+            Console.WriteLine("Form1_Paint");
 
-   // Draw the arc.
-            g.DrawArc(p, r, startAngle, sweepAngle);
-            Console.WriteLine("xxxx");
+            Graphics g = e.Graphics; 
+              
+           // g.Clear(Color.White);
+            var p = new Pen(Color.Black, 1);
+            rectOfDraw = new Rectangle(120, 10, this.Width - 140, this.Height - 50); 
+            g.DrawRectangle(p, rectOfDraw); 
+/**//*
+            foreach(var point in listPoints) {
+                Console.WriteLine("Draw point");
+                e.Graphics.DrawRectangle(p, point.X, point.Y, 10, 10);
+            }*/
+            if(listPoints.Count > 0) {
+
+                switch(mode) {
+                    case actions.DOT:
+                        break;
+                    case actions.DEFAULT:
+
+                        break;    
+                }
+                Console.WriteLine(listPoints);
+                g.FillClosedCurve(Brushes.ForestGreen, listPoints.ToArray());
+            }
+                
         }
 
         private void initializeComponent() {
@@ -57,13 +86,41 @@ namespace myApp {
                 b.Click += new EventHandler (Button_Click);
                 Controls.Add (b);
             } 
-
-            //Canvas myCanvas = new Canvas();
         }
  
+
+        private void Form1_Click(object sender, EventArgs e) {
+            if(e is MouseEventArgs) {
+                var me = (MouseEventArgs)e;
+                if(rectOfDraw.Contains(me.Location.X, me.Location.Y)) {
+                    Console.WriteLine("Click to rect");
+                    listPoints.Add(new PointF(me.X, me.Y)); //me.Location
+                }
+                Console.WriteLine(me.Location);
+                Refresh();
+            } 
+        }
+
         private void Button_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(sender.GetType().Name, e.ToString());
+            if(sender is Button) {
+                var b = (Button)sender;
+                switch(b.Text) {
+                    case "Точки":
+                        mode = actions.DOT;
+                        Refresh();
+                        break;
+                    case "Очистить":
+                        listPoints.Clear(); 
+                        mode = actions.CLEAR;
+                        Refresh();
+                        break;    
+                    default:
+                        mode = actions.DEFAULT;
+                        break;
+                }
+                //MessageBox.Show(b.Text);
+            }            
         }
     }
 }
