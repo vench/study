73f5f541309e6d3c3dc008bed049806d0c1f1ec5
vench <nowwrit@mail.ru>
2018-10-26 19:33:16 +0300

    2610

--- MyForm.cs
+++ MyForm.cs
@@ -98,19 +98,65 @@ namespace myApp {
         //barSeries.ChartType = SeriesChartType.Bar; 
        // barSeries.ChartTypeName = "xx";
        // chart.Series.Add(barSeries);
+
+        double[] xData = new double[] {1,2,3,4,5 };
+        double[] yData = new double[] {6,7,3,4,5 };
+
         chart.Location = new Point(0, wDataGridView.Bottom);
 
+        pictureBox.Location = new Point(wDataGridView.Right, wDataGridView.Top);
+        pictureBox.Size = new Size(propertyGrid.Width, wDataGridView.Height);
+        pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        pictureBox.ImageLocation = "https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg";
+        
+        //ChartArea area = new ChartArea("First");
+        
+       // chart.Series.Add("Series2");
+      //  chart.Series["Series2"].ChartType = SeriesChartType.Column;
+      //  chart.Series["Series2"].Points.AddY(20);
+       // chart.Series["Series2"].ChartArea = "ChartArea1";
+       // chart.ChartAreas.Add(area);
+        /* 
+            //Create a series using the data
+            Series barSeries = new Series();
+            barSeries.Points.DataBindXY(xData, yData);
+            //Set the chart type, Bar; horizontal bars
+            barSeries.ChartType = SeriesChartType.Bar;
+            //Assign it to the required area
+            barSeries.ChartArea = "First";
+            //Add the series to the chart
+            chart.Series.Add(barSeries);
+            //Vertical bar chart
+            //create another area and add it to the chart
+            ChartArea area2 = new ChartArea("Second");
+            chart.ChartAreas.Add(area2);
+            //Create the series using just the y data
+            Series barSeries2 = new Series();
+            barSeries2.Points.DataBindY(yData);
+            //Set the chart type, column; vertical bars
+            barSeries2.ChartType = SeriesChartType.Column;
+            barSeries2.ChartArea = "Second";
+            //Add the series to the chart
+            chart.Series.Add(barSeries2);
        // chart.Titles.Add("График");
         
        // this.chart
-    
+    */
     } 
    
 
 	private void Data_Grid_changeSelect(object sender, EventArgs e)
     { 
         propertyGrid.SelectedObject = wBindingNavigatorBindingSource.Current;
-        
+        var country = (Country)propertyGrid.SelectedObject;
+        if(country == null) {
+            return;
+        }
+        try { 
+            pictureBox.Load( country.ImageSrc);
+        } catch(Exception er) {
+            Console.WriteLine("Exception: " + er.Message);
+        } 
     }
 
     private void saveDataButton_Click(object sender, EventArgs e) { 
