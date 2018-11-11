576d32b99dd1a92651aee236014210c533527f7b
vench <nowwrit@mail.ru>
2018-11-11 10:09:46 +0300

    scissor example 1011

--- main.cpp
+++ main.cpp
@@ -3,36 +3,44 @@
 #include "all.h" 
  
  
-float speed = 4.;
-float angle = 0;
+float gx = 0.35f, gy = 0.58f, gw, gh;
 
 void displayMe(void) {
 
-        std::cout << "displayMe: " << angle << ", " << speed << std::endl; 
- 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
-        //
-        glPushMatrix();
-        meSolidCube(.1);
-        glPopMatrix();
-        //
-      
-        glPushMatrix();  
-       // glTranslated(0, 0, -20);
-       // glRotated(angle,0,0,1); 
-       glRotated(34,0,1,1);
-        glRotated(angle,1,0,0);
-       // glRotated(angle,0,1,0);
-        
-        meSolidCube(.3); 
-         glPopMatrix();
-         
-        glutSwapBuffers();
+        glClearColor(1, 1, 1, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_POINTS);
+	glColor3f(1, 0, 0);
+	glVertex2f(-0.2f, -0.2f);
+	glVertex2f(0, -0.2f);
+	glVertex2f(0, -0.1f);
+	glVertex2f(0.2f, -0.2f);
+	glVertex2f(0, 0.4f);
+	glEnd();
+	glEnable(GL_SCISSOR_TEST); // Включаем ножницы
+	glClearColor(0.9f, 0.8f, 0.7f, 0); // Меняем цвет фона
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POINTS);
+	glColor3f(0, 0, 1);
+	glVertex2f(-0.2f, 0.2f);
+	glVertex2f(0, 0.2f);
+	glVertex2f(0, 0.1f);
+	glVertex2f(0.2f, 0.2f);
+	glVertex2f(0, -0.4f);
+	glEnd();
+	glDisable(GL_SCISSOR_TEST); // Выключаем ножницы
+	glutSwapBuffers();
+	glFlush();
 }
 
 void reshapeMe(int w, int h) {
         std::cout << "Reshape" << std::endl;
+        
+        gw = w;
+	gh = h;
+	glScissor((int)(gx * w), (int)(gy * h), (int)(0.3f * w), (int)(0.3f * h));
+        
         glViewport(0,0,w,h);
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
@@ -41,34 +49,31 @@ void reshapeMe(int w, int h) {
 }
 
 void initOpenGl() {
-        glClearColor(1,1,1,0);
-        glShadeModel(GL_SMOOTH); //glShadeModel(GL_FLAT); 
-        glPointSize(7);
-        glEnable(GL_POINT_SMOOTH);
+        glClearColor(1, 1, 1, 0);
+	glShadeModel(GL_FLAT);
+	glPointSize(10);
+	glEnable(GL_POINT_SMOOTH);
 }
 
 //
 
-void onKeyboardFunc(int key, int x, int y )
-{
-
-        if (key == GLUT_KEY_UP) {
-                angle+=speed;
-                if(angle > 360) {
-                        angle = 0;
-                        if (speed < 20) speed +=.5;
-                } 
-        } else if (key == GLUT_KEY_DOWN) {
-                angle-=speed;
-                if(angle <= 0) {
-                        angle = 360;
-                        if (speed > 0) speed -=.5;
-                }        
-        }
-    
-        
+void onKeyboardFunc(unsigned char key, int x, int y )
+{         
         glutPostRedisplay();
 } 
+
+void onSpecialKey(int key, int x, int y ){
+        
+        switch(key) {
+	        case GLUT_KEY_LEFT: gx -= 0.005f; break;
+	        case GLUT_KEY_RIGHT: gx += 0.005f; break;
+	        case GLUT_KEY_DOWN: gy -= 0.005f; break;
+	        case GLUT_KEY_UP:  gy += 0.005f; break;
+	}
+	
+	glScissor((int)(gx * gw), (int)(gy * gh), (int)(0.3f * gw), (int)(0.3f * gh));
+	glutPostRedisplay();
+}
 
 //
 int main(int argc, char** argv) {
@@ -77,14 +82,14 @@ int main(int argc, char** argv) {
         std::cout << "Start" << std::endl;
 
         glutInit(&argc, argv);
-        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+        glutInitDisplayMode(GLUT_DOUBLE  );
         glutInitWindowSize(800, 600);
         glutInitWindowPosition(100,100);
         glutCreateWindow("Hello world");
-        glEnable(GL_DEPTH_TEST); // проверка на порядок !
+       // glEnable(GL_DEPTH_TEST); // проверка на порядок !
         initOpenGl();
         //glutKeyboardFunc(onKeyboardFunc);
-        glutSpecialFunc(onKeyboardFunc);
+        glutSpecialFunc(onSpecialKey);
         glutReshapeFunc(reshapeMe);
         glutDisplayFunc(displayMe);
         glutMainLoop();
