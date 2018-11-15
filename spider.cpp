2067859a7dde73fc04ae7a3169a0ccf61890aec4
vench <nowwrit@mail.ru>
2018-11-15 10:56:14 +0300

    lab 2 1411

--- spider.cpp
+++ spider.cpp
@@ -1,9 +1,11 @@
 #include "spider.h"
 
+
 Spider::Spider(QWidget *parent)
     : QWidget(parent)
 {
-
+    this->mouseDown = false;
+    this->setWindowTitle(tr("Spider window"));
 }
 
 Spider::~Spider()
@@ -11,4 +13,36 @@ Spider::~Spider()
 
 }
 
+//
+void Spider::mousePressEvent(QMouseEvent * event) {
 
+    if(event->button() == Qt::MouseButton::RightButton &&
+       (event->modifiers() & Qt::KeyboardModifier::ControlModifier) > 0) {
+        QString s = QString("X:%1,Y:%2")
+                .arg(QString::number(event->pos().x()),
+                     QString::number(event->pos().y()));
+
+        QToolTip::showText(event->globalPos(), s);
+    }
+    if(event->button() == Qt::MouseButton::RightButton ) {
+        this->mouseDown = true;
+    } else {
+        this->mouseDown = false;
+    }
+}
+
+//
+void Spider::mouseReleaseEvent(QMouseEvent*) {
+    this->mouseDown = false;
+}
+
+//
+void Spider::mouseMoveEvent(QMouseEvent * event) {
+   // qDebug() << event->pos();
+}
+
+
+//
+void Spider::repaint() {
+    qDebug() << "event->pos()";
+}
--- spider.h
+++ spider.h
@@ -3,16 +3,25 @@
 
 #include <QWidget>
 #include <QDebug>
+#include <QMouseEvent>
+#include <QToolTip>
 
 class Spider : public QWidget
 {
     Q_OBJECT
+
+    bool mouseDown;
 
 public:
     Spider(QWidget *parent = 0);
     ~Spider();
 
 
+protected:
+    void mousePressEvent( QMouseEvent * event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent * event );
+    void repaint();
 };
 
 #endif // SPIDER_H
