9ef11754dd3545d81eaca4eebdc3637912b4557a
vench <nowwrit@mail.ru>
2018-11-19 10:12:26 +0300

    update 1911

--- spider.cpp
+++ spider.cpp
@@ -5,7 +5,9 @@ Spider::Spider(QWidget *parent)
     : QWidget(parent)
 {
     this->mouseDown = false;
-    this->setWindowTitle(tr("Spider window"));
+    this->mouseRightDown = false;
+    this->updateTitleInfo();
+    this->changeCursorDefault();
 }
 
 Spider::~Spider()
@@ -16,28 +18,70 @@ Spider::~Spider()
 //
 void Spider::mousePressEvent(QMouseEvent * event) {
 
-    if(event->button() == Qt::MouseButton::RightButton &&
-       (event->modifiers() & Qt::KeyboardModifier::ControlModifier) > 0) {
-        QString s = QString("X:%1,Y:%2")
-                .arg(QString::number(event->pos().x()),
-                     QString::number(event->pos().y()));
+    if(event->button() == Qt::MouseButton::RightButton) {
 
-        QToolTip::showText(event->globalPos(), s);
-    }
-    if(event->button() == Qt::MouseButton::LeftButton ) {
+        if((event->modifiers() & Qt::KeyboardModifier::ControlModifier) > 0) {
+            QString s = QString("X:%1,Y:%2")
+                    .arg(QString::number(event->pos().x()),
+                         QString::number(event->pos().y()));
+
+            QToolTip::showText(event->globalPos(), s);
+        } else {
+            this->firstPos = event->pos();
+            this->mouseRightDown = true;
+        }
+
+    } else if(event->button() == Qt::MouseButton::LeftButton ) {
         this->mouseDown = true;
+        changeCursor(":spider.png");
     } else {
         this->mouseDown = false;
+        this->mouseRightDown = false;
     }
+}
+
+
+//
+void Spider::changeCursor(Qt::CursorShape s) {
+    this->setCursor(  QCursor(s));
+}
+
+
+//
+void Spider::changeCursor(QString s) {
+    QPixmap pix(s);
+    pix.setMask(QBitmap(s));
+    QCursor cr1(pix.scaled(28,28));
+    this->setCursor(cr1);
+}
+
+//
+void Spider::changeCursorDefault() {
+     this->changeCursor(Qt::BusyCursor);
 }
 
 //
 void Spider::mouseReleaseEvent(QMouseEvent*) {
     this->mouseDown = false;
+    if(this->mouseRightDown) {
+        QRect r;
+        this->fillRect(r);
+        listRects.append(r);
+    }
+    this->mouseRightDown = false;
+    this->changeCursorDefault();
 }
 
 //
 void Spider::mouseMoveEvent(QMouseEvent * event) {
+    if(!this->rect().contains(event->pos())) {
+        this->mouseReleaseEvent(event);
+        return;
+    }
+    if(this->mouseRightDown) {
+        lastPos = event->pos();
+        repaint();
+    }
     if(this->mouseDown) {
         lastPos = event->pos();
         repaint();
@@ -47,11 +91,50 @@ void Spider::mouseMoveEvent(QMouseEvent * event) {
 
 //
 void Spider::paintEvent ( QPaintEvent *  ) {
+    QPainter p(this);
+    if(this->mouseDown) {
+        this->drawWeb(&p);
+    }
+    if(this->mouseRightDown){
+        this->drawRect(&p);
+    }
+
+    this->drawRects(&p);
+}
+
+//
+void Spider::drawRects(QPainter*p) {
+    p->setPen(QPen(Qt::blue,1,Qt::SolidLine));
+    for(auto i : listRects) {
+        p->drawRect(i);
+    }
+}
+
+//
+void Spider::drawRect(QPainter*p) {
+    p->setPen(QPen(Qt::green,1,Qt::SolidLine));
+    QRect r;
+    this->fillRect(r);
+    p->drawRect(r);
+}
+
+//
+ void Spider::fillRect(QRect&r) {
+     int x = std::min(this->firstPos.x(), this->lastPos.x()),
+         y = std::min(this->firstPos.y(), this->lastPos.y()),
+         w = fabs(this->firstPos.x() - this->lastPos.x()),
+         h = fabs(this->firstPos.y() - this->lastPos.y());
+     r.setX(x);
+     r.setY(y);
+     r.setWidth(w);
+     r.setHeight(h);
+ }
+
+//
+void Spider::drawWeb(QPainter*p) {
     if(lastPos.x() <= 0 || lastPos.y() <= 0) {
         return;
     }
-    QPainter p(this);
-
     int lb[8][4] = {
         {lastPos.x(), lastPos.y(),0,0},
         {lastPos.x(), lastPos.y(),width()>>1,0},
@@ -63,21 +146,19 @@ void Spider::paintEvent ( QPaintEvent *  ) {
         {lastPos.x(), lastPos.y(),0,height() >> 1}
     };
 
-    p.setPen(QPen(Qt::red,1,Qt::SolidLine));
+    p->setPen(QPen(Qt::red,1,Qt::SolidLine));
 
     int a[2],b[2];
     mathCPoint(lb[7], a);
 
-
     for(int i = 0; i < 8; i ++) {
-        p.drawLine(lb[i][0], lb[i][1], lb[i][2], lb[i][3]);
+        p->drawLine(lb[i][0], lb[i][1], lb[i][2], lb[i][3]);
 
         mathCPoint(lb[i], b);
-        p.drawLine(a[0], a[1], b[0], b[1]);
+        p->drawLine(a[0], a[1], b[0], b[1]);
         a[0] = b[0];
         a[1] = b[1];
     }
-
 }
 
 //
@@ -87,4 +168,16 @@ void Spider::mathCPoint(int *line, int *pc) {
     double k = llen / len;
     pc[0] = line[0] + (line[2]-line[0])*k;
     pc[1] = line[1] + (line[3]-line[1])*k;
+}
+
+
+//
+void Spider::resizeEvent(QResizeEvent *) {
+    this->updateTitleInfo();
+}
+
+//
+void Spider::updateTitleInfo() {
+    QString s = tr("Spider window, width: %1, height: %2").arg(this->width()).arg(this->height());
+    this->setWindowTitle(s);
 }
--- spider.h
+++ spider.h
@@ -6,6 +6,7 @@
 #include <QMouseEvent>
 #include <QToolTip>
 #include <QPainter>
+#include <QBitmap>
 #include <math.h>
 
 class Spider : public QWidget
@@ -13,10 +14,21 @@ class Spider : public QWidget
     Q_OBJECT
 
     bool mouseDown;
+    bool mouseRightDown;
+    QPoint firstPos;
     QPoint lastPos;
+    QList<QRect> listRects;
 
 
     void mathCPoint(int *, int *);
+    void changeCursor(QString s);
+    void changeCursor(Qt::CursorShape s);
+    void changeCursorDefault();
+    void updateTitleInfo();
+    void drawWeb(QPainter*);
+    void drawRect(QPainter*);
+    void drawRects(QPainter*);
+    void fillRect(QRect&);
 
 public:
     Spider(QWidget *parent = 0);
@@ -28,6 +40,7 @@ protected:
     void mouseReleaseEvent(QMouseEvent *event);
     void mouseMoveEvent(QMouseEvent * event );
     void paintEvent ( QPaintEvent * event );
+    void resizeEvent(QResizeEvent *);
 };
 
 #endif // SPIDER_H
