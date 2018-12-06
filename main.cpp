b566cf86682b631685301f131ecbe41ffec26f1c
vench <nowwrit@mail.ru>
2018-12-06 10:27:51 +0300

    save 1207

--- main.cpp
+++ main.cpp
@@ -12,13 +12,18 @@ float
                 0, 0, 1, 0, 
                 0, 0, 0, 1 
                 },  
-	dx=0.1f, dy=.0, dz = -20.0f, 
+	dx=0.1f, dy=.0, dz = -10.0f, 
 	spec[] = {0.9f,0.9f,0.9f},
+	colorSun[] = {.7,.7,0,1},
+	colorEarth[] = {0,0,1,.5},
+	colorMoon[] = {0.5,0.5,0.5,1},
+	colorEndl[] = {0,0,0,1},
 	speed = .1;
-short posX, posY,deep; // Текущая позиция указателя мыши
-bool leftButton, twoSide, normale=1,deepType=0,normaleLine=0; 
+	
+short posX, posY,deep; 
+bool leftButton, twoSide, normale=1,deepType=0,normaleLine=0, sunRun,dayRun,yaerRun,moonRun; 
 int maxDeep = 6;
-float speedTest = 0.5;
+float speedTest = 1;
 
 //
 void addRotation(float angle, float x, float y, float z)
@@ -33,16 +38,17 @@ void addRotation(float angle, float x, float y, float z)
 
 //
 void displayMe(void) {
-       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPushMatrix();	// Запоминаем единичное значение ММ в стеке
 	glTranslated(dx, dy, dz);
 	glMultMatrixf(rotMatrix); // Изменяем ММ
-
-	glColor3f(1, 1, 1);
+ 
 	
 	glPushMatrix();
 	glRotatef(sun, 0,1,0);	// Вращаем систему координат
-	glutWireSphere(1,20,20);	// Рисуем Солнце
+	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, colorSun);
+	glutSolidSphere(1,20,20);	// Рисуем Солнце
+	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, colorEndl);
 	glPopMatrix();
         // sun end
         
@@ -51,27 +57,26 @@ void displayMe(void) {
 	glPushMatrix();
 	 
 	glRotatef(year, 0, 1, 0);
-	glTranslatef(20, 0, 0); 
+	glTranslatef(4, 0, 0); 
+	glPushMatrix(); 
+	 
 	glRotatef(day, 0, 1, 0);
 	
-
-	glutWireSphere(0.3,20,20); // Рисуем Землю
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, colorEarth);
+	glutSolidSphere(0.3,20,20); // Рисуем Землю
 	
-	// moon
-	glPushMatrix();
+	glPopMatrix();	
+	 
 	glRotatef(moon, 0, 1, 0);
 	glTranslatef(1, 0,  0); 
 	
-	glutWireSphere(0.08,20,20);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, colorMoon);
+	glutSolidSphere(0.08,20,20);
 	glPopMatrix();
 	// end moon
 	
 	glPopMatrix();
-	// earth end
-	 
-	
-	
-	glPopMatrix();		
+	// earth end 
 	
 	
 	
@@ -89,45 +94,92 @@ void reshapeMe(int w, int h) {
 }
 
 void initOpenGl() {
-        glClearColor(1, 1, 1, 0);
-	glShadeModel(GL_SMOOTH); //
-	glEnable(GL_DEPTH_TEST);  
+       
+	/*glShadeModel(GL_SMOOTH); //
+	 
 	glCullFace(GL_BACK);
 	glEnable(GL_CULL_FACE);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_COLOR_MATERIAL); 
+	
 	glMaterialfv(GL_FRONT, GL_SPECULAR, spec);
         glMateriali(GL_FRONT, GL_SHININESS, 128); 
-        glEnable( GL_LIGHTING); 
-        glLineWidth(3); 
+    
+        
+        glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);*/
+        
+        // 
+        float pos[4] = {0,0,0,1};
+        float dir[3] = {-1,-1,-1};
+
+    float mat_specular[] = {1,1,1,1 };
+        glEnable(GL_DEPTH_TEST);
+
+  glEnable(GL_COLOR_MATERIAL);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+
+    glLightfv(GL_LIGHT1, GL_POSITION, pos);
+    glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, dir);
+
+
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular); //Добавьте блеск всем телам:
+    glMaterialf(GL_FRONT, GL_SHININESS, 128.0);
+
+
+	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+       
         DrawScene();
 }
  
 
  
 
- 
-
-//
-void onKeyboardFunc(byte key, int x, int y )
-{       
-         
-	glutPostRedisplay();
-} 
-
-//
+ //
 void onIdle() {
         //addRotation(speedY * .1, 0, 1, 0);
         //addRotation(speedX * .1, 1, 0, 0);
         
-        sun += 0.01; 
-        moon += 2;
-        day += 0.3;
-        year += 0.1;
+        if(sunRun) sun += 0.01; 
+        if(moonRun) moon += 2;
+        if(dayRun) day += 0.3;
+        if(yaerRun) year += 0.1;
         glutPostRedisplay(); 
 }
+
+
+//
+void onKeyboardFunc(byte key, int x, int y )
+{       
+
+        switch(key){
+                case 's':
+                        sunRun = !sunRun;
+                break;
+                case 'd':
+                        dayRun = !dayRun;
+                break;
+                case 'm':
+                        moonRun = !moonRun;
+                break;
+                case 'y':
+                        yaerRun = !yaerRun;
+                break;
+        }
+        
+        if(sunRun || dayRun || moonRun || yaerRun) {
+                glutIdleFunc(onIdle);
+        } else {
+        
+        }
+         
+	glutPostRedisplay();
+} 
+
 
 //
 void onSpecialKey(int key, int x, int y ){
