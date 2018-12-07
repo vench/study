2722baadf3038a771527410508d684fe1952aa5e
vench <nowwrit@mail.ru>
2018-12-07 17:13:51 +0300

    save

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,19 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->lineEdit->setValidator(new QIntValidator(0, 100));
+    ui->horizontalSlider->setToolTip(tr("Scale %"));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::setTextEditLine(const QString & val) {
+
+    qDebug() << "xxxxx";
+    emit changeTextEditLine(val.toInt());
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QIntValidator>
+#include <QDebug>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +16,12 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+public Q_SLOTS:
+    void setTextEditLine(const QString &);
+
+Q_SIGNALS:
+    void changeTextEditLine( int );
 
 private:
     Ui::MainWindow *ui;
